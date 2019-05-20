15a77ddcc95488b9b238f3d0594726384fe0df65
tuanpham <pham.anh.tuan@framgia.com>
2019-05-20 15:43:42 +0700

    api admin category

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,8 +5,18 @@ class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   scope :load_categories, ->{select :id, :name}
+  scope :api_load_categories, ->{select(:id, :name).map{|ca| ca.load_structure}}
 
   def get_image
-    images.blank? ? "category.png" : images.first.image.thumb.url
+    images.blank? ? "category.png" : images.first.image.url
+  end
+
+  def load_structure
+    result = {
+      id: id,
+      name: name,
+      image: get_image
+    }
+    result
   end
 end
