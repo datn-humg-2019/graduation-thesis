b1446880e500017bb65d1f6d39b231ce9f46dc31
Pham Anh Tuan <pham.anh.tuan@sun-asterisk.com>
2019-07-08 15:45:27 +0700

    multiple description products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: :show_product_public
-  before_action :get_product, only: :show_product
+  before_action :get_product, only: [:show_product, :edit]
 
   def new
     @product = Product.new
@@ -8,13 +8,42 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new product_params
-    if @product.save
-      save_images params[:product][:image]
-      save_pw @product.id
+    if params[:product][:image].nil?
+      flash[:danger] = "Bạn chưa lựa chọn ảnh"
+      render :new
+    else
+      @product.description = "Không có mô tả"
+      if @product.save
+        save_pw @product.id
+        save_images params[:product][:image]
+        redirect_to warehouses_path
+      else
+        flash[:danger] = t ".create_fail"
+        render :new
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    pw = current_user.check_has_product(params[:id]).has_description.first
+    pw.description = params[:product_warehouse][:description]
+
+    if pw.save
+      images = params[:product_warehouse][:image]
+      unless images.blank?
+        images.each do |i|
+          image = pw.images.build
+          image.image = i
+          image.save
+        end
+      end
+      flash[:success] = "Cập nhật thành công"
       redirect_to warehouses_path
     else
-      flash[:danger] = t ".create_fail"
-      render :new
+      flash[:danger] = "Cập nhật thất bại"
+      render edit_product_path(id: params[:id])
     end
   end
 
@@ -24,9 +53,8 @@ class ProductsController < ApplicationController
 
   def show_product_public
     user = User.find_by id: params[:u_id]
-    @pw = user.check_has_product(params[:p_id]).last if user
-
-    return if @pw
+    @pws = user.check_has_product(params[:p_id]) if user
+    return unless @pws.blank?
     respond_to do |format|
       format.html{render file: "#{Rails.root}/public/500", layout: false, status: :error}
       format.xml{head :not_found}
@@ -51,15 +79,16 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit :name, :tag, :description, :category_id
+    params.require(:product).permit :name, :tag, :category_id
   end
 
   def save_images param
     images = param
     count = 0
     unless images.blank?
+      @product.images.build(image: images.first).save
       images.each do |i|
-        image = @product.images.build
+        image = @pw.images.build
         image.image = i
         count += 1 unless image.save
       end
@@ -72,21 +101,23 @@ class ProductsController < ApplicationController
   end
 
   def save_pw product_id
-    pw = current_user.warehouse.product_warehouses.build
-    pw.product_id = product_id
-    pw.count = params[:count_pw]
-    pw.price_origin = convert_price params[:price_origin_pw]
-    pw.price_sale = convert_price params[:price_sale_pw]
-    pw.mfg = params[:mfg_pw].present? ? convert_date(params[:mfg_pw]) : nil
-    pw.exp = params[:exp_pw].present? ? convert_date(params[:exp_pw]) : nil
-    pw.stop_providing = false
-    pw.save
-    pw.save_history
+    @pw = current_user.warehouse.product_warehouses.build
+    @pw.product_id = product_id
+    @pw.count = params[:count_pw]
+    @pw.price_origin = convert_price params[:price_origin_pw]
+    @pw.price_sale = convert_price params[:price_sale_pw]
+    @pw.mfg = params[:mfg_pw].present? ? convert_date(params[:mfg_pw]) : nil
+    @pw.exp = params[:exp_pw].present? ? convert_date(params[:exp_pw]) : nil
+    @pw.stop_providing = false
+    @pw.description = params[:product][:description]
+    @pw.save
+    @pw.save_history
   end
 
   def get_product
     @product = Product.find_by id: params[:id]
     @pws = current_user.check_has_product params[:id]
+    @images = @pws.map(&:images).flatten!
 
     return if @product
     flash[:danger] = t "user_not_found"
